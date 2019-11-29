dc77cd56760c217df09b27237850730cc0b2c048
Pedro Palhari <pedropalhari@gmail.com>
2019-11-29 12:56:53 -0300

    Adicionado overlap por grid-area também

--- README.md
+++ README.md
@@ -81,21 +81,18 @@ Basicamente, definir um grid, deixar umas colunas a mais com template, e usar s√
   display: grid;
 
   grid-template-areas:
-    "d d d d"
-    "a a a a"
-    "b b b b"
-    "c c c c";
+    ". d d . ."
+    "a a a a ."
+    "b b b b ."
+    "c c c c .";
 
-  grid-template-columns: repeat(4, minmax(0, 50px));
+  grid-template-columns: repeat(5, minmax(0, 50px));
   grid-template-rows: repeat(4, minmax(0, 50px));
   border-radius: 20px;
   overflow: hidden;
-
-  box-shadow: 0 3px 0px rgba(0, 0, 0, 0.16), 0 3px 0px rgba(0, 0, 0, 0.23);
 }
 
 /* filhos com template-area */
-
 #overlap-a {
   grid-area: a;
   background-color: red;
@@ -109,17 +106,34 @@ Basicamente, definir um grid, deixar umas colunas a mais com template, e usar s√
 #overlap-c {
   grid-area: c;
   background-color: blue;
+  border-bottom-right-radius: 20px;
+  border-bottom-left-radius: 20px;
 }
-
-/* circulo em cima de x filhos, com margin pra ficar menor que o tamanho da linha */
 #overlap-circle {
-  grid-row: 1 / 3;
-  grid-column: 2 / 4;
+  /* inicial linha, inicial coluna */
+  /* final linha, final coluna (intervalo aberto)*/
+  grid-area: d / d / a / d;
+
   background-color: grey;
   opacity: 0.5;
   border-radius: 100px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   margin: 15px;
+}
+#overlap-x {
+  /* por n√∫meros */
+  grid-area: 1 / 4 / 3 / 6;
+  background-color: black;
+  border-radius: 100px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  margin: 30px;
+  margin-right: 40px;
+  margin-top: 40px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
 }
 ```
 
--- src/App.svelte
+++ src/App.svelte
@@ -74,19 +74,18 @@
     display: grid;
 
     grid-template-areas:
-      "d d d d"
-      "a a a a"
-      "b b b b"
-      "c c c c";
+      ". d d . ."
+      "a a a a ."
+      "b b b b ."
+      "c c c c .";
 
-    grid-template-columns: repeat(4, minmax(0, 50px));
+    grid-template-columns: repeat(5, minmax(0, 50px));
     grid-template-rows: repeat(4, minmax(0, 50px));
     border-radius: 20px;
     overflow: hidden;
-
-    box-shadow: 0 3px 0px rgba(0,0,0,0.16), 0 3px 0px rgba(0,0,0,0.23);
   }
 
+  /* filhos com template-area */
   #overlap-a {
     grid-area: a;
     background-color: red;
@@ -100,15 +99,34 @@
   #overlap-c {
     grid-area: c;
     background-color: blue;
+    border-bottom-right-radius: 20px;
+    border-bottom-left-radius: 20px;
   }
   #overlap-circle {
-    grid-row: 1 / 3;
-    grid-column: 2 / 4;
+    /* inicial linha, inicial coluna */
+    /* final linha, final coluna (intervalo aberto)*/
+    grid-area: d / d / a / d;
+
     background-color: grey;
     opacity: 0.5;
     border-radius: 100px;
-    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
     margin: 15px;
+  }
+  #overlap-x {
+    /* por números */
+    grid-area: 1 / 4 / 3 / 6;
+    background-color: black;
+    border-radius: 100px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    margin: 30px;
+    margin-right: 40px;
+    margin-top: 40px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
   }
 </style>
 
@@ -133,6 +151,8 @@
       <div id="overlap-c" />
 
       <div id="overlap-circle" />
+
+      <div id="overlap-x">x</div>
     </div>
   </div>
 </div>
